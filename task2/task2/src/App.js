a26dd50b54557718b109172a643df549174f9403
yusufkhan-0912 <yusufdkhan11@gmail.com>
2023-12-04 19:33:01 +0530

    optimized

--- task2/task2/src/App.js
+++ task2/task2/src/App.js
@@ -2,70 +2,41 @@ import React, { useState } from "react";
 // import Button from "./Button";
 
 let App = () => {
-  let [message, setMessage] = useState(false);
-  let [messaget, setMessaget] = useState(false);
-  let [messageThree, setMessageThree] = useState(false);
-  let [messageFour, setMessageFour] = useState(false);
-  let [messageFive, setMessageFive] = useState(false);
+  let [message, setMessage] = useState([false, false, false, false, false]);
 
-  let onclick = () => {
-    setMessage(!message);
-    setMessaget(false);
-    setMessageThree(false);
-    setMessageFour(false);
-    setMessageFive(false);
+  let buttonClicked = (index) => {
+    let updateMessage = message.map((message, i) =>
+      i === index ? !message : false
+    );
+    setMessage(updateMessage);
   };
 
-  let clickT = () => {
-    setMessaget(!messaget);
-    setMessage(false);
-    setMessageThree(false);
-    setMessageFour(false);
-    setMessageFive(false);
-  };
-  let ClickThree = () => {
-    setMessageThree(!messageThree);
-    setMessage(false);
-    setMessaget(false);
-    setMessageFour(false);
-    setMessageFive(false);
-  };
-
-  let clickFour = () => {
-    setMessageFour(!messageFour);
-    setMessage(false);
-    setMessaget(false);
-    setMessageThree(false);
-    setMessageFive(false);
-  };
-  let clickFive = () => {
-    setMessageFive(!messageFive);
-    setMessage(false);
-    setMessaget(false);
-    setMessageThree(false);
-    setMessageFour(false);
-  };
+  let Text = [
+    "This is tower",
+    "The top house",
+    "The Bubble house",
+    "The Hell Road",
+    "Click to get the full View",
+  ];
 
   return (
     <>
-      <img src="/pic.png" alt="img" />
-
-      <button className="btn1" onClick={onclick}>
-        +{message && <p>This is tower</p>}
-      </button>
-
-      <button className="btn2" onClick={clickT}>
-        + {messaget && <p>The top house</p>}
-      </button>
-      <button className="btn3" onClick={ClickThree}>
-        + {messageThree && <p>The Bubble house </p>}
-      </button>
-      <button className="btn4" onClick={clickFour}>
-        + {messageFour && <p>The Hell Road</p>}
-      </button>
-      <button className="btn5" onClick={clickFive}>
-        + {messageFive && <p>Click to get the full View</p>}
-      </button>
+      <video controls width="600" height="400" autoPlay>
+        <source src="./pic.mp4" type="video/mp4" />
+      </video>
+      {message.map((message, index) => {
+        return (
+          <button
+            key={index}
+            className={`btn${index + 1}`}
+            onClick={() => {
+              buttonClicked(index);
+            }}
+          >
+            +{message && <p>{Text[index]}</p>}
+          </button>
+        );
+      })}
     </>
   );
 };
